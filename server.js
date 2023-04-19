1a8732de3e890fa54549dc6dd312d26f0c026297
nickycraig <nickycraig@gmail.com>
2023-04-19 16:39:19 -0700

    added delete ejs pages

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const methodOverride = require('method-override');
 const portlandController = require('./controllers/portland');
 const newyorkController = require('./controllers/newyork');
 const neworleansController = require('./controllers/neworleans');
-
 const PORT = 5001;
 
 app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({ extended:false }));
 app.use(express.static(__dirname + '/Public'));
+app.use(methodOverride('_method'));
 
 app.get('/', (req, res) => {
     res.render('home.ejs')
